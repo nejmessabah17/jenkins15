cee331bc0e3b94b145f5b741d033a67589176a8d
nejmessabah17 <nbouchra998@gmail.com>
2023-05-30 21:19:20 -0400

    minor change

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
 
     public static void main(String[] args) throws InterruptedException {
-        for (int i=0;i<10;i++){
+        for (int i=0;i<20;i++){
             System.out.println("hello");
             Thread.sleep(2000);
         }
